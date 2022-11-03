194efe8bb20045d66904f0cf5a17f931853595d4
Nargess Heydari <Nheydari@uwaterloo.ca>
2022-11-03 16:20:48 -0400

    Set Matrix Zeros added from desktop

---
+++ Set Matrix Zeros.py
@@ -0,0 +1,13 @@
+def setZeroes(self, matrix):
+    R = len(matrix)
+    C = len(matrix[0])
+    rows, cols = set(), set()
+    for i in range(R):
+        for j in range(C):
+            if matrix[i][j] == 0:
+                rows.add(i)
+                cols.add(j)
+    for i in range(R):
+        for j in range(C):
+            if i in rows or j in cols:
+                matrix[i][j] = 0
